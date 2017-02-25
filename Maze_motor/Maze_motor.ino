e2b0ff2deed4344c58e137f862f44db8b1a8632f
Chris Dileo <chris.dileo@gmail.com>
2017-02-24 17:29:46 -0800

    Maze code now working

--- Maze_motor/Maze_motor.ino
+++ Maze_motor/Maze_motor.ino
@@ -6,28 +6,27 @@
  * Natalie Ngan
  * Steven Kim
 */
-
-#include <NewPing.h> // For enabling our ultrasonic sensors
+#include <NewPing.h>
 /*Define Motor Pins. R/L indicates left/right motor and F/R indicates whether 
                   the robot moves forward or reverse when powered on pin.*/
 #define motorRF 10 //A-iB
 #define motorRR 9 //A-iA
 #define motorLF 8 
 #define motorLR 7 
-#define touchSensor 6
-
-#define R_TRIGGER_PIN  1
-#define R_ECHO_PIN     2
-#define L_TRIGGER_PIN  3
-#define L_ECHO_PIN     4
+#define touchSensor 13
+#define TRIGGER_PIN_L 5
+#define ECHO_PIN_L 6
+#define TRIGGER_PIN_R 3
+#define ECHO_PIN_R 4
 #define MAX_DISTANCE 200
-#define SENSOR_DISTANCE_THRESHOLD 5
- 
-NewPing r_sonar(R_TRIGGER_PIN, R_ECHO_PIN, MAX_DISTANCE);
-NewPing l_sonar(L_TRIGGER_PIN, L_ECHO_PIN, MAX_DISTANCE);
+#define THRESHOLD 20
+#define TURN_TIME 450 //in ms
+#define THINKING_TIME 1000 //in ms 
+#define BACK_TIME 300 //ms
+#define MOTOR_SPEED 255 // 0-255
 
-float distToRight = -1.0;
-float distToLeft = -1.0;
+NewPing sonarL(TRIGGER_PIN_L, ECHO_PIN_L, MAX_DISTANCE);
+NewPing sonarR(TRIGGER_PIN_R, ECHO_PIN_R, MAX_DISTANCE);
 
 void setup() {
   pinMode(motorRF, OUTPUT);
@@ -36,53 +35,96 @@ void setup() {
   pinMode(motorLR, OUTPUT);
   pinMode(touchSensor, INPUT);
   Serial.begin(9600); // Start reading form our touch sensor
+  pinMode(TRIGGER_PIN_L, OUTPUT);
+  pinMode(ECHO_PIN_L, INPUT);
+  pinMode(TRIGGER_PIN_R, OUTPUT);
+  pinMode(ECHO_PIN_R, INPUT);
 }
 
 void loop(){ //starts random motor movement for testing
   // TODO: make a state machine for robot behaviours
-//  testMovement();
-  //Navigation code
-  //Assuming one ultrasonic sensor on L and R sides, each loop, take a reading of distances
-  distToRight = getSensorReading('R');
-  distToLeft = getSensorReading('L');
+//  if (readLeftSensor() > 20) {
+//    turnLeft();
+//  } else if (readRightSensor() > 20) {
+//    turnRight();
+//  } else {
+//    goForward();
+//  }
 
-  // If one side is farther +/- a threshold value, move towards it
-  if (distToRight > distToLeft + SENSOR_DISTANCE_THRESHOLD) {
-    // TODO: Turn Right? Or should we try to gradually turn?
-    turnRight();
-  } else if (distToRight + SENSOR_DISTANCE_THRESHOLD < distToLeft) {
-    turnLeft();
-  } else {
-    // Keep going straight
-    goForward();
+  goForward();
+  delay(50); //TODO: remove me, just testing a force forward
+  //check for a bump
+  Serial.println(digitalRead(touchSensor));
+  if (checkTouch() == 0) {
+    backUpAndLookForHole();
   }
-  
 }
 
-float getSensorReading(char side) {
-  float value = -1.0;
-  if (side == 'L'){
-    value = l_sonar.ping_cm();
-  } else if (side == 'R') {
-    value = r_sonar.ping_cm();
-  }
-  return value;
+int checkTouch() {
+  return (int)digitalRead(touchSensor);
 }
 
-void testMovement() {
-  goForward();
-  delay(1000);
-  turnLeft();
-  delay(1000);
-  goForward();
-  delay(1000);
-  turnRight();
-  delay(1000);
+void backUpAndLookForHole() {
+  //read left and right sensors
+  int distLeft;
+  int distRight;
   stopMotors();
-  delay(1000);
+  delay(THINKING_TIME);
   goBackward();
-  delay(1000);
+  delay(BACK_TIME);
+  
+  while (true) {
+    distLeft = readLeftSensor();
+    distRight = readRightSensor();
+    if (distLeft > distRight && distLeft > THRESHOLD){
+      goBackward();
+      delay(BACK_TIME);
+      turnLeft(255);
+      delay(TURN_TIME);
+      break;
+    } else if (distRight > distLeft && distRight > THRESHOLD){
+      goBackward();
+      delay(BACK_TIME);
+      turnRight(255);
+      delay(TURN_TIME);
+      break;
+    } else {
+      goBackward();
+      delay(BACK_TIME);
+      //runaway possibililty
+    }
+  }  
+  stopAndThink();
 }
+
+void stopAndThink() {
+  stopMotors();
+  delay(THINKING_TIME);
+}
+
+int readLeftSensor() {
+  digitalWrite(TRIGGER_PIN_L, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(TRIGGER_PIN_L, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN_L, LOW); 
+
+  int duration = pulseIn(ECHO_PIN_L, HIGH);
+  return duration*0.034/2;
+}
+int readRightSensor() {
+  digitalWrite(TRIGGER_PIN_R, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(TRIGGER_PIN_R, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN_R, LOW); 
+
+  int duration = pulseIn(ECHO_PIN_R, HIGH);
+  return duration*0.034/2;
+}
+
 
 void stopMotors(){
   // To Avoid doubling up signals on a motor, stop everything between motor behaviours
@@ -94,26 +136,26 @@ void stopMotors(){
 
 void goForward() {
   stopMotors();
-  analogWrite(motorRF, 255); 
-  analogWrite(motorLF, 255); 
+  analogWrite(motorRF, MOTOR_SPEED); 
+  analogWrite(motorLF, MOTOR_SPEED); 
 }
 
 void goBackward() {
   stopMotors();
-  analogWrite(motorRR, 255); 
-  analogWrite(motorLR, 255); 
+  analogWrite(motorRR, MOTOR_SPEED); 
+  analogWrite(motorLR, MOTOR_SPEED); 
 }
 
-void turnLeft() {
+void turnLeft(int speed) {
   stopMotors();
-  analogWrite(motorRF, 255); 
-  //analogWrite(motorLR, 255); // Enable me for a tighter turn
+  analogWrite(motorRF, speed); 
+//  analogWrite(motorLR, MOTOR_SPEED); // Enable me for a tighter turn
 }
 
-void turnRight() {
+void turnRight(int speed) {
   stopMotors();
-  //analogWrite(motorRR, 255); // Enable me for a tighter turn
-  analogWrite(motorLF, 255); 
+//  analogWrite(motorRR, MOTOR_SPEED); // Enable me for a tighter turn
+  analogWrite(motorLF, speed); 
 }
 
 
